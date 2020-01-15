f001e62f6cb2ea01ec74a6c8ac7fbd9940c7a5cb
Lucy <lucymidgley13@gmail.com>
2020-01-15 22:19:41 +0000

    Added standard input to our funtion to accept input from cli

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
 const connect  = require('./client');
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function(){
+    stdin.on('data', (key) => {if (key === '\u0003') {
+      process.exit();
+    }
+    });
+  }
+  handleUserInput();
+  return stdin;
+}
 
 console.log('Connecting ...');
 connect();
+setupInput();
