dfd3fa322e1c15e28c2fc440afa34316e0c43e2f
Lucy <lucymidgley13@gmail.com>
2020-01-15 22:01:52 +0000

    added some comands to move the snake upon connection

--- client.js
+++ client.js
@@ -9,7 +9,21 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to the server!');
     conn.write("Name: LM" )
-  });
+});
+// conn.on('connect', () => {setTimeout(() => { 
+//   conn.write("Move: up" );
+// }, 1000);
+//   });
+//   conn.on('connect', () => {setTimeout(() => { 
+//     conn.write("Move: left" );
+//   }, 2000);
+//     });
+//     conn.on('connect', () => {setTimeout(() => {
+//       setInterval(() => { 
+//       conn.write("Move: left" );
+//     }, 100); 
+//     }, 2500);
+//       });
   conn.on('data', (data) => {
     console.log(data);
   });
