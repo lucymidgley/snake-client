5818ee4ae2f12c9d382e8f437444a514f894626f
Lucy <lucymidgley13@gmail.com>
2020-01-15 22:55:57 +0000

    Allowed our connection to recieve input in the form of WASD reffering to movements of the snake

--- input.js
+++ input.js
@@ -1,20 +1,38 @@
+let connection;
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
+
   
 const handleUserInput = function(){
     stdin.on('data', (key) => {if (key === '\u0003') {
       process.exit();
     }
+    else if (key === 'w') {
+      connection.write("Move: up");
+    }
+    else if (key === 'a') {
+      connection.write("Move: left");
+    }
+    else if (key === 's') {
+      connection.write("Move: down");
+    }
+    else if (key === 'd') {
+      connection.write("Move: right");
+    }
     });
+    
   }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   
   stdin.resume();
  
   handleUserInput();
   return stdin;
 }
+
+
 
 module.exports = setupInput;
