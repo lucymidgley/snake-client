ad4ace87cb95f1f0e439813b5dabb2f6d216c30a
Lucy <lucymidgley13@gmail.com>
2020-01-15 22:29:11 +0000

    Created input file and imported the standard input functions from there

--- play.js
+++ play.js
@@ -1,18 +1,5 @@
 const connect  = require('./client');
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = function(){
-    stdin.on('data', (key) => {if (key === '\u0003') {
-      process.exit();
-    }
-    });
-  }
-  handleUserInput();
-  return stdin;
-}
+const setupInput = require('./input');
 
 console.log('Connecting ...');
 connect();
